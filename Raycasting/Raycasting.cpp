77fc6429b443f1d6a38544077be05fcc159ca993
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-08-14 16:45:32 -0500

    Wrote the code for the raycasting. Next is the code to control the camera and player's positions.

--- Raycasting/Raycasting.cpp
+++ Raycasting/Raycasting.cpp
@@ -1,20 +1,150 @@
-// Raycasting.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Raycasting.cpp 
+// By: Sam Schmitz
+
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
 
-int main()
+#define mapWidth 24
+#define mapHeight 24
+#define screenWidth 640
+#define screenHeight 480
+
+int worldMap[mapWidth][mapHeight] = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+    {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+    {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+int main(int /*argc*/, char */*argv*/[])
 {
-    std::cout << "Hello World!\n";
+    double posX = 22, posY = 12;    //Player's starting location
+    double dirX = -1, dirY = 0;     //Player's starting direction
+    double planeX = 0, planeY = 0.66;   //Player's camera plane
+
+    double time = 0;    //time of the current frame
+    double oldTime = 0;     //time of the previous frame
+
+    screen(screenWidth, screenHeight, 0, "Raycaster");
+    while (!done()) {
+        for (int x = 0; x < w; x++) {
+            // Calculate ray position and direction
+            double cameraX = 2 * x / double(w) - 1;
+            double rayDirX = dirX + planeX * cameraX;
+            double rayDirY = dirY + planeY * cameraX;
+
+            // which box of the map we're in 
+            int mapX = int(posX);
+            int mapY = int(posY);
+
+            //length of ray from current position to next x or y-side
+            double sideDistX;
+            double sideDistY;
+
+            //length of ray from one x or y-side to next x or y-side
+            double deltaDistX = (rayDirX == 0) ? 1e30 : std::abs(1 / rayDirX);
+            double deltaDistY = (rayDirY == 0) ? 1e30 : std::abs(1 / rayDirY);
+            double perpWallDist;    //used to calc the len of the ray
+
+            //what direction to step in x or y-direction (either +1 or -1)
+            int stepX;
+            int stepY;
+
+            int hit = 0;    //was there a wall hit?
+            int side;   //was a NS or EW wall hit?
+
+            //calculate step and initial sideDist
+            if (rayDirX < 0) {
+                stepX = -1;
+                sideDistX = (posX - mapX) * deltaDistX;
+            }
+            else {
+                stepX = 1;
+                sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+            }
+
+            if (rayDirY < 0) {
+                stepY = -1;
+                sideDistY = (posY - mapY) * deltaDistY;
+            }
+            else {
+                stepY = 1;
+                sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+            }
+
+            //perform DDA
+            while (hit == 0) {
+                // jump to next map square either in x-dir or y-dir
+                if (sideDistX < sideDistY) {
+                    sideDistX += deltaDistX;
+                    mapX += stepX;
+                    side = 0;
+                }
+                else {
+                    sideDistY += deltaDistY;
+                    mapY += stepY;
+                    side = 1;
+                }
+                // Check if ray has hit a wall
+                if (worldMap[mapX][mapY] > 0) hit = 1;
+            }
+
+            // Calculate distance projected on camera direction (Euclidean distance would give a fisheye effect)
+            if (side == 0) perpWallDist = (sideDistX - deltaDistX);
+            else perpWallDist = (sideDistY - deltaDistY);
+
+            //calculate height of line to draw on screen
+            int lineHeight = (int)(h / perpWallDist);
+
+            //calculate lowest and highest pixel to fill in current stage
+            int drawStart = -lineHeight / 2 + h / 2;
+            if (drawStart < 0) drawStart = 0;
+            int drawEnd = lineHeight / 2 + h / 2;
+            if (drawEnd >= h) drawEnd = h - 1;
+
+            //choose wall color
+            ColorRGB color;
+            switch (worldMap[mapX][mapY]) {
+            case 1: color = RGB_Red; break;
+            case 2: color = RGB_Green; break;
+            case 3: color = RGB_Blue; break;
+            case 4: color = RGB_White; break;
+            default: color = RGB_Yellow; break;
+            }
+
+            //give x and y sides different brightness
+            if (side == 1) { color = color / 2; }
+
+            //draw the pixeld of the stripe as a vertical line
+            verLine(x, drawStart, drawEnd, color);
+        }
+
+
+    }
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
